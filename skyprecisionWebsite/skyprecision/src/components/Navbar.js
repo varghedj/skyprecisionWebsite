1515f59e1418cafeff23ab3337cbf6577180b357
unknown <dylanvarghese@gmail.com>
2021-07-21 04:07:00 -0400

    navbar links and login button

--- skyprecisionWebsite/skyprecision/src/components/Navbar.js
+++ skyprecisionWebsite/skyprecision/src/components/Navbar.js
@@ -2,11 +2,24 @@ import React,  { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import {ReactComponent as Icon } from './icon.svg'
+import { Button } from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setbutton] = useState(true);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setbutton(false)
+    } else {
+      setbutton(true)
+    }
+  }
+
+  window.addEventListener('resize', showButton);
 
   return (
     <>
@@ -19,6 +32,36 @@ function Navbar() {
           <div className="menu-icon" onClick={handleClick}>
             {click ? <FaTimes /> : <FaBars />}
           </div>
+          <ul classname={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to='/' className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/about' className="nav-links">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/team' className="nav-links">
+                Team
+              </Link>
+            </li>
+            <li className="nav-btn">
+              {button ? (
+                <Link to='/log-in' className="btn-link">
+                  <Button buttonStyle='btn-outline'>LOG-IN</Button>
+                </Link>
+              ) : (
+                <Link to='/log-in' className="btn-link">
+                  <Button buttonStyle='btn-outline' buttonSize='btn-mobile'>
+                    LOG-IN
+                  </Button>
+                </Link>
+              )}
+            </li>
+          </ul>
         </div>
       </div>
     </>
